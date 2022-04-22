49104f3c1f2c868b7198514bfdafa298bc353c7d
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-04-22 21:20:13 +0300

    feat: full functionality

---
+++ src/components/ConfirmPopup.js
@@ -0,0 +1,25 @@
+//Not Needed?
+
+// import PopupWithForm from "./PopupWithForm";
+// import React from "react";
+
+// function ConfirmPopup({ isOpen, onClose, onConfirmSubmit, card }) {
+//   function handleSubmit(e) {
+//     e.preventDefault();
+
+//     onConfirmSubmit(card);
+//   }
+
+//   return (
+//     <PopupWithForm
+//       name="confirm"
+//       title="Are you sure?"
+//       submitButton="Yes"
+//       isOpen={isOpen}
+//       onClose={onClose}
+//       onSubmit={handleSubmit}
+//     />
+//   );
+// }
+
+// export default ConfirmPopup;
