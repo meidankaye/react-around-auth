df2aff03320ab2c5e0d90421c2f8a49b908a0675
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-04-22 21:59:46 +0300

    fix: api name

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import AddPlacePopup from "./AddPlacePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import React from "react";
-import { api } from "../utils/Api";
+import api from "../utils/api";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
