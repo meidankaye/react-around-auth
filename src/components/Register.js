bdf4c0b89a3a6be77d8616304a5672de2db9567e
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-06-04 15:55:28 +0300

    feat: register start

---
+++ src/components/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+import headerLogo from "../images/header-logo.png";
+import "./styles/Register.css";
+
+function Register() {
+    return(
+        <div className="register">
+            
+        </div>
+    )
+}
---
+++ src/utils/auth.js
@@ -0,0 +1,22 @@
+export const BASE_URL = "https://register.nomoreparties.co";
+
+export const register = (username, password, email) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password, email }),
+  })
+    .then((response) => {
+      try {
+        if (response.status === 200) {
+          return response.json();
+        }
+      } catch (e) {
+        return e;
+      }
+    })
+    .catch((err) => console.log(err));
+};
