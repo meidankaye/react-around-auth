efa4a309bc8acb21649e1dd041ad1d4347ca1f98
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-06-06 22:19:50 +0300

    register progress

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link, withRouter, useState, useHistory } from "react-router-dom";
+import * as auth from "../utils/auth";
 import "./styles/Register.css";
 
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    auth.register(password, email).then((res) => {
+      if (res) {
+        history.push("/signin");
+      } else {
+        console.log("Something went wrong.");
+      }
+    });
+  };
 
   return (
     <div className="register">
@@ -14,6 +29,7 @@ function Register() {
           placeholder="Email"
           type="email"
           value={email}
+          onChange={(e) => setEmail(e.target.value)}
           required
         ></input>
         <input
@@ -21,9 +37,16 @@ function Register() {
           placeholder="Password"
           type="text"
           value={password}
+          onChange={(e) => setPassword(e.target.value)}
           required
         ></input>
+        <button className="register__button" type="submit" onSubmit={handleSubmit}>
+          Sign Up
+        </button>
       </form>
+      <p className="register__message">Already a member? Log in here!</p>
     </div>
   );
 }
+
+export default withRouter(Register);
